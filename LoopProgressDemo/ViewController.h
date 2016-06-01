48a8efa71721f07c81474e0756ecc9c57af75299
MHZ <wlsxmahaozhe@163.com>
2016-06-01 17:08:28 +0800

    Initial Commit

---
+++ LoopProgressDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoopProgressDemo
+//
+//  Created by 马浩哲 on 16/6/1.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoopProgressDemoTests/LoopProgressDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  LoopProgressDemoTests.m
+//  LoopProgressDemoTests
+//
+//  Created by 马浩哲 on 16/6/1.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoopProgressDemoTests : XCTestCase
+
+@end
+
+@implementation LoopProgressDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
